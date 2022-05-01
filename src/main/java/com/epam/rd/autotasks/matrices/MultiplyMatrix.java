4f369b4ccab8f7bcdab79cf75ac0d955c5ba2b34
Matthew Zhigalo <matthewzhigalo@gmail.com>
2022-05-01 23:58:50 +0300

    solved

--- src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/MultiplyMatrix.java
@@ -3,10 +3,33 @@ import java.util.Arrays;
 
 public class MultiplyMatrix {
     public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
-
         // Put your code here
+        int rows1 = 0;
+        int col1rows2 = 0;
+        int col2 = 0;
+        for (int i = 0; i< matrix1.length; i++) {
+            rows1 = matrix1.length;
+            for (int j = 0; j < matrix1[0].length; j++) {
+                col1rows2 = matrix1[0].length;
+            }
+        }
+        for (int i = 0; i < matrix2.length; i++) {
+            for (int j = 0; j < matrix2[0].length; j++) {
+                col2 = matrix2[0].length;
+            }
+        }
+        int[][] resultMatrix = new int[rows1][col2];
+        for (int i = 0; i < rows1; i++) {
+            for (int j = 0; j < col2; j++) {
+                int r = 0;
+                while (r != col1rows2) {
+                    resultMatrix[i][j] += matrix1[i][r] * matrix2[r][j];
+                    r++;
+                }
+            }
+        }
 
-        return null;
+        return resultMatrix;
     }
 
     public static void main(String[] args) {
